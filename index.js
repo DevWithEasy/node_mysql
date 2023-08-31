7946235b37c8b184c551105d8a17245df596ef91
DevWithEasy <devwitheasy@gmail.com>
2023-08-31 17:49:48 +0600

    relation two table concept

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const errorHandler = require('./middlewares/errorHandler');
 const applyMidleware = require('./middlewares/middlewares');
 const {PrismaClient} = require('@prisma/client')
+const bcrypt = require('bcrypt');
 
 const prisma = new PrismaClient()
 
@@ -13,25 +14,97 @@ applyMidleware(app)
 
 
 app.get('/',async(req,res)=>{
-  const alluers = await prisma.user.findMany()
-  res.json(alluers)
+  try {
+    const alluers = await prisma.user.findMany()
+    res.json(alluers)
+  } catch (error) {
+    res.json({
+      error : error.message
+    })
+  }
 })
 
-app.post('/',async(req,res)=>{
-  const newData = await prisma.user.create({
-    data : req.body
-  })
-  res.json(newData)
+app.post('/user/',async(req,res)=>{
+  try {
+    const {password} = req.body
 
-  const createMany = await prisma.user.createMany({
-    data: [
-      { name: 'Bob', email: 'bob@prisma.io' },
-      { name: 'Bobo', email: 'bob@prisma.io' }, // Duplicate unique key!
-      { name: 'Yewande', email: 'yewande@prisma.io' },
-      { name: 'Angelique', email: 'angelique@prisma.io' },
-    ],
-    skipDuplicates: true, // Skip 'Bobo'
-  })
+    const hashed = await bcrypt.hash(password , 10)
+
+    const user = await prisma.user.create({
+      data : {
+        ...req.body,
+        password : hashed,
+        image : '/image/users/user_default.png'
+      }
+    })
+
+    res.json(user)
+  
+  } catch (error) {
+    res.json({
+      error : error.message
+    })
+  }
+
+})
+
+app.put('/:id',async(req, res)=>{
+  try {
+    const updateData = await prisma.user.update({
+      where: {
+        id : parseInt(req.params.id)
+      },
+      data : {
+        name : req.body.name,
+        email : req.body.email
+      }
+    })
+    res.json(updateData)
+  } catch (error) {
+    res.json({
+      error : error.message
+    })
+  }
+})
+
+app.get('/contact/:id',async(req,res)=>{
+  try {
+    const contacts = await prisma.contact.findMany({
+      where : {
+        authorId : req.params.id
+      },
+      include : {
+        author : true
+      }
+    })
+
+    res.json(contacts)
+  
+  } catch (error) {
+    res.json({
+      error : error.message
+    })
+  }
+
+})
+
+app.post('/contact/',async(req,res)=>{
+  try {
+    const user = await prisma.contact.create({
+      data : {
+        ...req.body,
+        image : '/image/users/user_default.png'
+      }
+    })
+
+    res.json(user)
+  
+  } catch (error) {
+    res.json({
+      error : error.message
+    })
+  }
+
 })
 
 //error handler
